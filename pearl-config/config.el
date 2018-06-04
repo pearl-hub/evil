fdf0f571eb45b4391a0e0936b8d44ee4d8d8f4e2
Filippo Squillace <squillace.filippo@gmail.com>
2018-06-04 23:29:46 +1000

    Enable `C-u` scroll

--- pearl-config/config.el
+++ pearl-config/config.el
@@ -1,7 +1,10 @@
 
 
 (setq pearl-pkgvardir (file-name-as-directory (getenv "PEARL_PKGVARDIR")))
-
 (add-to-list 'load-path (concat pearl-pkgvardir "evil"))
+
+; Enable C-u scroll
+(setq evil-want-C-u-scroll t)
+
 (require 'evil)
 (evil-mode 1)
